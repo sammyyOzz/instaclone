aac06bd78dc33fa0b1ca47fe1d203c65892291a6
Sammy <sammyoziegbe@gmail.com>
2020-06-24 11:52:37 +0100

    About to add image required

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+	public function create()
+	{
+		return view('posts.create');
+	}
+
+	public function store()
+	{
+		$data = request()->validate([
+			'caption' => 'required',
+			'image' => 'required',
+		]);
+
+		dd(request()->all());
+	}
 }
